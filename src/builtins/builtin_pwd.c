0d0720e28d5ff84d5baa050210ca973b8688f821
Ko-HyeJi <hy2ji.k5@gmail.com>
2022-09-17 07:45:41 +0900

    fixed builtin pwd

--- src/builtins/builtin_pwd.c
+++ src/builtins/builtin_pwd.c
@@ -4,14 +4,15 @@
 
 #define	BUFFER_SIZE 1024
 
-int	builtin_pwd()
+int	builtin_pwd(t_script *cur_cmd)
 {
 	char	*buf;
 
 	buf = getcwd(NULL, BUFFER_SIZE);
 	if (!buf)
 		return (-1);
-	printf("%s\n", buf);
+	ft_putstr_fd(buf, cur_cmd->fd_out);
+	ft_putstr_fd("\n", cur_cmd->fd_out);
 	free(buf);
 	return (0);
 }
