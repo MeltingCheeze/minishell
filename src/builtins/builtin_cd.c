8acd000df9ba519ceff0c4ceffdf14ae8a31955a
Ko-HyeJi <hy2ji.k5@gmail.com>
2022-09-17 07:25:52 +0900

    fixed builtin cd

--- src/builtins/builtin_cd.c
+++ src/builtins/builtin_cd.c
@@ -12,7 +12,9 @@ int	builtin_cd(char **argv, t_env_info *env_info)
 		path = find_env_value(env_info->head, "HOME");
 	if (chdir(path) < 0) // 실패
 	{
-		printf("cd: %s: no such file or directory\n", path);
+		ft_putstr_fd("cd: ", 2);
+		ft_putstr_fd(path, 2);
+		ft_putstr_fd(": no such file or directory\n", 2);
 		return (-1);
 	}
 	else // 성공 -> env $PWD, $OLDPWD 변경
