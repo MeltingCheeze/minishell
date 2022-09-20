6d22159020552c7f0c34bf9d2f225812cb77f0a2
chaejin-jen <jen71324@gmail.com>
2022-09-20 18:00:59 +0900

    FIX : tokenizer

--- src/tokenizer/tokenizer.c
+++ src/tokenizer/tokenizer.c
@@ -11,57 +11,59 @@ static int	destroy_token(t_token **token, char *line, int cnt)
 	return (258);
 }
 
-static int	add_token(t_token **token, char **start, char **cur, size_t len)
+static int	count_special_char(char *str)
 {
 	int	cnt;
 
+	if (cnt != '<' && cnt != '>' && cnt != '|')
+		return (0);
 	cnt = 1;
-	if (ft_strchr(SEPS, **cur))
-		tokenadd_back(token, tokennew(ft_substr(*start, 0, len)));
-	else
-	{
-		while (**cur == *(*cur + cnt))
-			cnt++;
-		if (cnt > 2 || (cnt > 1 && (**cur == '|')))
-		{
-			destroy_token(token, *cur, cnt);
-			return (1);
-		}
-		if (len && !ft_strchr(SEPS, **cur))
-			tokenadd_back(token, tokennew(ft_substr(*start, 0, len)));
-		tokenadd_back(token, tokennew(ft_substr(*cur, 0, cnt)));
-	}
-	*start += len + cnt;
-	while (**start && ft_strchr(SEPS, **start))
-		(*start)++;
-	*cur = *start - 1;
-	return (0);
+	while (*str == *(str + cnt))
+		cnt++;
+	//if (cnt > 2 || (cnt > 1 && (*str == '|')))
+	//	//destroy_token(token, str, cnt);
+	//	cnt = -1;
+	return (cnt);
 }
 
-static void	make_token(t_token **token, char *cur)
+static void	add_tokens(t_token **token, char *start, char *cur, int cnt)
+{
+
+	if (start != cur)
+		tokenadd_back(token, tokennew(ft_substr(start, 0, cur - start)));
+	if (cnt)
+		tokenadd_back(token, tokennew(ft_substr(cur, 0, cnt)));
+}
+
+
+static void	check_line(t_token **token, char *line)
 {
 	char	quote;
-	char	*start;
+	char	*cur;
+	int		cnt;
 
 	quote = 0;
-	while (*cur && ft_strchr(SEPS, *cur))
-		cur++;
-	start = cur;
+	if (*line == 0)
+		return ;
+	while (*line && ft_strchr(SEPS, *line))
+		line++;
+	cur = line;
 	while (*cur)
 	{
 		if (!quote && (*cur == '"' || *cur == '\''))
 			quote = *cur;
 		else if (quote && (quote == *cur))
 			quote = 0;
-		else if (!quote && ft_strchr(DELIMS, *cur))
+		else if (quote == 0 && ft_strchr(DELIMS, *cur))
 		{
-			if (add_token(token, &start, &cur, cur - start) != 0)
-				return ;
+			cnt = count_special_char(cur);
+			add_tokens(token, line, cur, cnt);
+			return (check_line(token, cur + cnt));
 		}
 		cur++;
 	}
-	if (cur - start)
-		tokenadd_back(token, tokennew(ft_strdup(start)));
+	if (cur - line)
+		tokenadd_back(token, tokennew(ft_strdup(line)));
 }
 
 t_token	*tokenizer(char *line)
@@ -69,6 +71,6 @@ t_token	*tokenizer(char *line)
 	t_token	*token;
 
 	token = 0;
-	make_token(&token, line);
+	check_line(&token, line);
 	return (token);
 }
