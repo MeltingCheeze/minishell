fd342317abd28301ebf5a0c143d5a26441bd1593
chaejin-jen <jen71324@gmail.com>
2022-09-21 19:40:31 +0900

    FIX : export, free_pptr

--- libft/src/ft_free_pptr.c
+++ libft/src/ft_free_pptr.c
@@ -6,7 +6,7 @@
 /*   By: chaejkim <chaejkim@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/17 15:39:58 by chaejkim          #+#    #+#             */
-/*   Updated: 2022/09/15 03:25:25 by chaejkim         ###   ########.fr       */
+/*   Updated: 2022/09/21 18:11:53 by chaejkim         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,8 @@ void	ft_free_pptr(void ***pptr)
 	int	i;
 
 	i = 0;
+	if (*pptr == 0)
+		return ;
 	while ((*pptr)[i])
 	{
 		free((*pptr)[i]);
