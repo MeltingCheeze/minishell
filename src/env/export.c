fbf61478ac172b6126e206c5ad11d15bd9020aa3
chaejin-jen <jen71324@gmail.com>
2022-09-26 14:48:33 +0900

    FIX rm init_pwd_env

--- src/env/export.c
+++ src/env/export.c
@@ -6,7 +6,7 @@
 /*   By: chaejkim <chaejkim@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/23 16:52:59 by hyko              #+#    #+#             */
-/*   Updated: 2022/09/24 14:52:00 by chaejkim         ###   ########.fr       */
+/*   Updated: 2022/09/26 14:31:51 by chaejkim         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,22 +21,6 @@ void	update_env(t_env *env, char *envp)
 	if (env->value)
 		free(env->value);
 	env->value = value;
-}
-
-t_env	*init_pwd_env(char *envp)
-{
-	t_env	*pwd_env;
-	char	*value;
-	char	*tmp;
-
-	value = getcwd(NULL, 0);
-	tmp = ft_strjoin(envp, "=");
-	envp = ft_strjoin(tmp, value);
-	pwd_env = env_new(envp);
-	free(value);
-	free(tmp);
-	free(envp);
-	return (pwd_env);
 }
 
 void	export_new(t_env_info *env_info, char *envp)
@@ -54,10 +38,7 @@ void	export_new(t_env_info *env_info, char *envp)
 		update_env(new_env, envp);
 		return ;
 	}
-	if (!ft_strcmp(envp, "PWD"))
-		new_env = init_pwd_env(envp);
-	else
-		new_env = env_new(envp);
+	new_env = env_new(envp);
 	env_add_back(&env_info->head, new_env);
 	env_info->size++;
 }
